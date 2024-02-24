f26eac300dc29174ee4f61dcf842a84f6953c4a7
mcastrod08 <mcastro08@gmail.com>
2024-02-24 16:03:40 -0400

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from "../utils/signature2.png"
+import logo from "../utils/signature.png"
 
 const Footer = () => {
   return (
     <div style={{color:"#fff", textAlign: 'center'}}> 
       © 2024 Copyrights: 
-      <img src={logo} alt="logo" width='60px'/>
+      <img src={logo} alt="logo" width='60px' style={{marginLeft:'0.3rem'}}/>
     </div>
   )
 }
